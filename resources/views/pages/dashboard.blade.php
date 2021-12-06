dd5bf201fadad034f466a89e3459838497864d2d
mkchemist <m.k_chemist@yahoo.com>
2021-12-06 06:40:59 +0200

    updating api url in dashboard.blade

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -13,7 +13,8 @@
     <input type="hidden" id="user" value="{{ $user }}">
     <input type="hidden" id="api_token" value="{{ $user->api_token }}">
     <input type="hidden" id="csrf_token" value="{{ csrf_token() }}">
-    <input type="hidden" id="app_base_api_url" value="{{ env('APP_BASE_API_URL') }}">
+    {{-- <input type="hidden" id="app_base_api_url" value="{{ env('APP_BASE_API_URL') }}"> --}}
+    <input type="hidden" id="app_base_api_url" value="{{ url("/")."/api" }}">
     <input type="hidden" id="app_base_uri" value="{{ env('APP_BASE_URI') }}">
     <div id="app"></div>
 @endsection
